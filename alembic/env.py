b9e0eee1c83fcdb9858f5b13dacaaa2f2d1aebe3
LuizFernando110 <luizloona@gmail.com>
2025-05-17 15:53:28 -0300

    Feature: últimas configurações

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-from app.db.base import Base  
+from app.db.base import Base
+
+load_dotenv()  
 
 config = context.config
 
