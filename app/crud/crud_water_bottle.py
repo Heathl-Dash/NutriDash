688646de14d8a646194802dd2d2c1738f7e6dfe1
LuizFernando110 <luizloona@gmail.com>
2025-06-02 16:56:55 -0300

    Feature: Adicionado mecânica de token nos endpoints de water bottle.

--- app/crud/crud_water_bottle.py
+++ app/crud/crud_water_bottle.py
@@ -12,12 +12,12 @@ def get_water_bottle(db: Session, water_bottle_id: int):
 def get_water_bottle_user(db: Session, user_id: int):
   return db.query(WaterBottle).filter(WaterBottle.user_id == user_id).all()
 
-def create_water_bottle(db: Session, water_bottle: WaterBottleCreate):
-  user_goal = get_water_goal_by_user(db, water_bottle.user_id)
+def create_water_bottle(db: Session, water_bottle: WaterBottleCreate, user_id: int):
+  user_goal = get_water_goal_by_user(db, user_id)
   if not user_goal:
     raise HTTPException(status_code=400, detail="Water goal not found for user")
 
-  db_water_bottle = WaterBottle(**water_bottle.model_dump(), water_goal_id=user_goal.water_goal_id)
+  db_water_bottle = WaterBottle(**water_bottle.model_dump(), user_id=user_id ,water_goal_id=user_goal.water_goal_id)
   db.add(db_water_bottle)
   db.commit()
   db.refresh(db_water_bottle)
