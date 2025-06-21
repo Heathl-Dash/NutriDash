dcc34575572a6e406ba772922a99fbbc513e0a93
LuizFernando110 <luizloona@gmail.com>
2025-06-21 12:11:34 -0300

    Fix: Concertada a rota de IA

--- app/api/routers/nutrition_info_ai_request.py
+++ app/api/routers/nutrition_info_ai_request.py
@@ -3,20 +3,20 @@ import json
 from fastapi import APIRouter, HTTPException
 from dotenv import load_dotenv
 from app.schemas.askAliment import AskAliment
-import openai
-import httpx
+from openai import OpenAI
+import logging
+import re
 
 load_dotenv()
 
 router = APIRouter()
 
-client = openai.OpenAI(
+client = OpenAI(
     api_key=os.getenv("OPENROUTER_API_KEY"),
-    base_url="https://openrouter.ai/api/v1",
-    http_client=httpx.Client()
+    base_url="https://openrouter.ai/api/v1"
 )
 
-@router.post("/nutrition_info_ai_request")
+@router.post("/nutrition-info-ai-request")
 def ask_nutritional_info(data: AskAliment):
     system_prompt = (
         "Você é um especialista em nutrição. "
@@ -25,21 +25,31 @@ def ask_nutritional_info(data: AskAliment):
         "Exemplo de resposta: "
         '{"calories": "89 kcal", "glycemic_index": 36, "vitamins": ["A", "B"], '
         '"good_for": "Bom para diabéticos", "bad_for": "Ruim para quando está privado"}'
+        "Todas as respostas contidas no JSON, devem ser em português, incluindo os minerais."
     )
 
     user_question = f"Me informe os dados nutricionais desse alimento: {data.aliment}"
 
     try:
         response = client.chat.completions.create(
-            extra_body = {},
-            model = "deepseek/deepseek-r1-0528:free",
+            model="mistralai/mistral-small-3.2-24b-instruct:free",
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_question},
             ],
             temperature=0.2,
+            extra_body={}
         )
         text_answer = response.choices[0].message.content
-        return json.loads(text_answer)
+        cleaned_json = re.sub(
+            r"```json\s*(.*?)```", r"\1",
+            text_answer,
+            flags=re.DOTALL 
+        ).strip()
+        try:
+            return json.loads(cleaned_json)
+        except json.JSONDecodeError:
+            raise HTTPException(status_code=502, detail="Resposta do modelo não está em formato JSON válido")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        logging.error(f"Erro ao chamar OpenAI: {e}")
+        raise HTTPException(status_code=500, detail="Erro ao consultar o modelo de IA.")
