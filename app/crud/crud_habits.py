59217155210e698989f95baff9bb6efa14e1c135
LuizFernando110 <luizloona@gmail.com>
2025-08-03 22:30:17 -0300

    feature

--- app/crud/crud_habits.py
+++ app/crud/crud_habits.py
@@ -28,6 +28,8 @@ def get_habits(
     if negative is True:
         query = query.filter(Habit.negative == True)
 
+    print(query)
+
     return query.order_by(Habit.created.desc()).offset(skip).limit(limit).all()
 
 
